7e8fb7c1822a9c394f7ff04a97f27088e36aba35
senir lovedale <126605344+senir-23@users.noreply.github.com>
2023-05-17 18:24:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,9 @@ def page_model():
         'weathersit': weather,
         'atemp': temp_normalized,
         'hum': humidity_normalized,
-        'windspeed':windspeed_normalized
+        'windspeed':windspeed_normalized,
+        'comfortable_temp' : comf_temp(temp_normalized),
+        'comfortable_humidity' : comf_hum(humidity_normalized)
     }
 
     input_df = pd.DataFrame([input_data])
